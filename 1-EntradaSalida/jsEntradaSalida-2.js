e381a5a421fb2a8637685f0b01c244c32729d257
julietaL <40776524+julietaL@users.noreply.github.com>
2018-07-02 22:03:50 -0300

    clase numero 1
    ejercicios del 1 al 4

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+var nombre;
+nombre="Julieta";
+alert("su nombre es + natalia natalia")
+nombre=prompt("natalia natalia");
+/* var person = prompt("Please enter your name", "Harry Potter");*/
+/*este ejercicio salio de w3schools*/
+
 }
+
 
