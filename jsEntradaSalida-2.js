fe7a84073a9f05bda090a1f104836c01875e1f7a
julietaL <40776524+julietaL@users.noreply.github.com>
2018-07-02 22:00:39 -0300

    clase numero 1
    julieta hasta ejercicio 4

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+var nombre;
+nombre="Julieta";
+alert("su nombre es + natalia natalia")
+nombre=prompt("natalia natalia");
+/* var person = prompt("Please enter your name", "Harry Potter");*/
+/*este ejercicio salio de w3schools*/
+
+}
+
+
