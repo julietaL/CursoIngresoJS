55d00442457a0b74644ba799a5a475af8c406955
julietaL <40776524+julietaL@users.noreply.github.com>
2018-07-07 12:12:36 -0300

    sabado
    eje 8 y 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,18 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+//:sueldo  //id:sueldo
+//salida:sueldo con el aumento //id:resultado
+var sueldo;
+var aumento;
+var sueldoConAumento;
+
+sueldo = document.getElementById('sueldo').value;
+sueldo = parseInt(sueldo);
+aumento = sueldo*(0.1);
+sueldoConAumento = sueldo + aumento; 
+document.getElementById('resultado').value = sueldoConAumento;
+//id:sueldo
+//id:salida
 	
 }
