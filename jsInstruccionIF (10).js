8821398f37b49222a259fa19c0b900fabb3abc20
julietaL <40776524+julietaL@users.noreply.github.com>
2018-07-14 13:04:23 -0300

    Add files via upload
    sabado

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,26 @@
+function mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+var nota;
+ nota = Math.floor((Math.random()*10)+1);
+
+if (nota>=9)
+{
+	alert("Excelente");
+}
+else
+{
+	if(nota >=4)
+
+	{
+	alert("Aprobo");
+	}
+	
+else
+ 	{
+	alert("vamos la proxima se puede");
+	}
+
+ 	}
+	}
+//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,12 @@
+function mostrar()
+{
+//tomo la edad  
+var edad
+edad=document.getElementById('edad').vale;
+edad=parseInt(edad);
+if((edad>=13) && (edad <=17))
+
+
+
+
+}//FIN DE LA FUNCIÓN
